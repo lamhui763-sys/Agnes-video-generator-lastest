3b82a53dd3ac8954ac2ea3c709ebdbc490056209
lamhui763-sys <lamhui763@gmail.com>
2026-07-19 14:55:52 +0800

    chore: aggressive cleanup

--- fix_app4.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-
-// Use regex to remove any floating ");" between </div> and {activeProject.scenes.length === 0
-code = code.replace(/<\/div>\s*\);\s*\{activeProject\.scenes\.length === 0/g, '</div>\n                        {activeProject.scenes.length === 0');
-
-fs.writeFileSync('src/App.tsx', code);
